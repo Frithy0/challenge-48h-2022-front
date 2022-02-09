43d3a7b6f89cfb7864c043ca0d77cbb4bbb1ffba
Louis Teilliais <louis.teilliais@ynov.com>
2022-02-09 02:03:47 +0100

    questions quiz

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -3,37 +3,37 @@ import { ref } from 'vue'
 import StarwarsService from "../services/module/starwars"
 const questions = ref([
     {
-        question: "What species, the creatures from Endor, who helped the rebellion destroying the second death star, were ?",
+        question: "Which species from Endor, helped the rebellion destroy the second death star ?",
         response: "Wookies",
-        categorie: "people",
+        categorie: "species",
         userResponse: null
     },
     {
         question: "What's Anakin Skywalker son's name ?",
-        response: "Luke",
+        response: "Luke Skywalker",
         categorie: "people",
         userResponse: null
     },
     {
-        question: "What sith Anakin Skywalker becomes ?",
-        response: 'Dark Vador',
+        question: "What's Anakin Skywalker's name in the sith  ?",
+        response: 'Darth Vader',
         categorie: "people",
         userResponse: null
     },
     {
         question: "What's the name of Luke Skywalker's sister ?",
-        response: 'Leia',
+        response: 'Leia Organa',
         categorie: "people",
         userResponse: null
     },
     {
-        question: "Who was obi wan kenobi's master ?",
+        question: "Who was Obi-Wan Kenobi's master ?",
         response: 'Yoda',
         categorie: "people",
         userResponse: null
     },
     {
-        question: "On wich planet, Anakin Skywalker was born ?",
+        question: "On wich planet was Anakin Skywalker born ?",
         response: 'Tatooine',
         categorie: "planets",
         userResponse: null
@@ -41,23 +41,23 @@ const questions = ref([
     {
         question: "What species does Jar Jar Binks belong to?",
         response: 'Gungans',
-        categorie: "people",
+        categorie: "species",
         userResponse: null
     },
     {
-        question: "Where does Obi-wan took Luke right after is birth ?",
+        question: "Where does Obi-wan take Luke right after his birth ?",
         response: 'Tatooine',
         categorie: "planets",
         userResponse: null
     },
     {
-        question: "What's the name of Boba Fett's starships ?",
+        question: "What's the name of Boba Fett's starship ?",
         response: 'Esclave I ',
         categorie: "starships",
         userResponse: null
     },
     {
-        question: "What's the name of Han Solo's starships ?",
+        question: "What's the name of Han Solo's starship ?",
         response: 'Faucon Millenium',
         categorie: "starships",
         userResponse: null
@@ -75,13 +75,13 @@ async function findFakeAnswer() {
     answers.value = await StarwarsService.getAnswers('people', questions.value[currentQuestion.value].response)
 }
 
-function next(){
-    
-    if (currentQuestion.value == questions.value.length-1){
+function next() {
+
+    if (currentQuestion.value == questions.value.length - 1) {
         return endgame.value = true
     }
 
-    if (questions.value[currentQuestion.value].response == questions.value[currentQuestion.value].userResponse){
+    if (questions.value[currentQuestion.value].response == questions.value[currentQuestion.value].userResponse) {
         winCounter.value++
     }
     currentQuestion.value++
@@ -96,7 +96,7 @@ findFakeAnswer()
 <template>
     <div class="container">
         <div v-if="!endgame" class="text-center">
-            <h2 style="color: white">{{currentQuestion}}/{{ questions.length }}</h2>
+            <h2 style="color: white">{{ currentQuestion }}/{{ questions.length }}</h2>
             <div class="card text-center center" style="width: 26rem;">
                 <div class="card-header">{{ questions[currentQuestion].question }}</div>
                 <ul class="list-group list-group-flush">
@@ -107,13 +107,18 @@ findFakeAnswer()
                     >{{ res }}</li>
                 </ul>
             </div>
-            <button @click="next()" :disabled="!questions[currentQuestion].userResponse" style="width: 26rem;" :class="{'btn': true, 'btn-warning': true, 'btn-block': true, 'mt-1': true, disabled: !questions[currentQuestion].userResponse}">Next 🡢</button>
+            <button
+                @click="next()"
+                :disabled="!questions[currentQuestion].userResponse"
+                style="width: 26rem;"
+                :class="{ 'btn': true, 'btn-warning': true, 'btn-block': true, 'mt-1': true, disabled: !questions[currentQuestion].userResponse }"
+            >Next 🡢</button>
         </div>
         <div class="text-center" v-else>
-            <div class="card" style="width: 15rem; margin-right: auto;margin-left: auto;">
-                You got {{ winCounter }}/{{ questions.length }}
-            </div>
-            
+            <div
+                class="card"
+                style="width: 15rem; margin-right: auto;margin-left: auto;"
+            >You got {{ winCounter }}/{{ questions.length }}</div>
         </div>
     </div>
 </template>
